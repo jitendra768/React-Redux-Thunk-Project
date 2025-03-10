dabfaf15dea283b4a198559f8df9ca1d9515fe2c
jitendra <jackjitendra768@gmail.com>
2025-03-10 22:52:47 +0530

    updated

--- src/Javascript/Settimeout/Debounce.js
+++ src/Javascript/Settimeout/Debounce.js
@@ -1,2 +1,14 @@
-// Debounce ensure a funtion executes only after a period of inactivity. 
+// Debounce is a technique that delays funtion execution until after a specified time has passed since the last event.
+// 1. prevent unnecessary API calls
+// 2. improve performance
+// 3. Enhance user experience
 
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+debounce()
